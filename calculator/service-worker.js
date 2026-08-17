6e2db048866d3dab693da6f55d1844340605ad39
graceyiyo-cloud <graceyiyo@gmail.com>
2026-08-17 17:55:20 +0800

    Compact calculator keypad

--- calculator/service-worker.js
+++ calculator/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'standalone-calculator-v4';
+const CACHE = 'standalone-calculator-v5';
 const FILES = ['./', './index.html', './app.js', './manifest.webmanifest', './icon.svg', './icon-180.png', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
